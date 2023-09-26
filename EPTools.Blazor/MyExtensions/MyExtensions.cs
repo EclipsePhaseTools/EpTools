144898e878492564de1c68d5025dab3a5c7e6790
Josiah Bradbury <herrozerro@gmail.com>
2023-09-26 14:04:04 -0500

    Added All files

---
+++ EPTools.Blazor/MyExtensions/MyExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPTools.Blazor.MyExtensions
+{
+    public static class MyExtensions
+    {
+		public static string ReplaceSubstring(this string s, int index, int length, string replacement)
+		{
+			var builder = new StringBuilder();
+			builder.Append(s.AsSpan(0, index));
+			builder.Append(replacement);
+			builder.Append(s.AsSpan(index + length));
+			return builder.ToString();
+		}
+	}
+}
